3cb5898c5030a713596457e5eba79141b7350dc9
Student_GA126 <student_ga126@psxga126.lan>
2020-08-17 15:32:14 -0400

    inspirationl quotes using API

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import axios from 'axios';
 
 class LandingPage extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
-            url: '',
+            url: 'https://type.fit/api/quotes',
+            info: [],
             quote: '',
+            author: '',
         }
     }
 
-    
+    componentDidMount() {
+        axios.get(this.state.url)
+            .then(res => {
+                let randomNum = Math.ceil(Math.random() * 1643)
+                this.setState({
+                    info: res.data,
+                    quote: res.data[randomNum].text,
+                    author: res.data[randomNum].author
+                })
+                // console.log(randomNum);
+                console.log(this.state.info);
+                console.log(this.state.quote);
+                console.log(this.state.author);
+            })
+            .catch(err => console.log(err.message))
+    }
+
     render() {
         return (
             <div className="LandingPage">
                 <h3>This is the landing page</h3>
+                <div className='quote'>
+                    <p>{this.state.quote}</p>
+                    <p>{this.state.author}</p>
+                </div>
             </div>
         )
     }
