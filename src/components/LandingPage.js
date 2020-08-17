76382dae3c57d494a86aac71651d5e1a01a31a96
Student_GA126 <student_ga126@psxga126.lan>
2020-08-17 11:02:56 -0400

    Assets + components

---
+++ src/components/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return(
+        <div>
+            <h3>This is the landing page</h3>
+        </div>
+    )
+}
+
+export default LandingPage;
