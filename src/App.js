23323250eea61d6a5ab11d2288afedd84d5c664e
Student_GA126 <student_ga126@psxga126.lan>
2020-08-17 16:42:49 -0400

    added meditate component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import LandingPage from './components/LandingPage';
+import Meditate from './components/Meditate';
 
 function App() {
   return (
     <div className="App">
       <h4>Navbar will go up here</h4>
       <LandingPage />
+      <Meditate />
     </div>
   );
 }
---
+++ src/components/Meditate.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Meditate = () => {
+    return(
+        <div className='Meditate'>
+            <h3>This is where the meditation will take place</h3>
+        </div>
+    )
+}
+
+export default Meditate;
