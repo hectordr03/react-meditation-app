fb07e259f6963146f2b2430058df90f0bac0e96c
Student_GA126 <student_ga126@psxga126.lan>
2020-08-20 16:36:31 -0400

    Timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -7,17 +7,51 @@ class Timer extends React.Component {
         super(props)
 
         this.state = {
-            timerIsOn: true,
-            minutes: '5',
-            seconds: '00',
+            timerStart: false,
+            minutes: 5,
+            seconds: 0,
         }
+    }
+
+    startTimer() {
+        this.setState({
+            timerStart: true,
+        }, console.log(`State of timer => ${this.state.timerStart}`))
+    }
+
+    stopTimer() {
+        this.setState({
+            timerStart: false,
+        }, console.log(`State of timer => ${this.state.timerStart}`))
+    }
+
+    addMin() {
+        let minCount = this.state.minutes;
+        this.setState({
+            minutes: minCount + 1,
+        })
+    }
+
+    minusMin() {
+        let minCount = this.state.minutes;
+        this.setState({
+            minutes: minCount - 1,
+        })
+    }
+
+    timer() {
+        
     }
 
     render() {
         return (
             <div className='Timer'>
                 <h4>How long would you like to meditate for?</h4>
-
+                <button onClick={() => this.addMin()}>+</button>
+                <p>Minutes: {this.state.minutes}:{this.state.seconds}</p>
+                <button onClick={(() => this.minusMin())}>-</button>
+                <button onClick={() => this.startTimer()}>Start Timer</button>
+                <button onClick={() => this.stopTimer()}>Stop Timer</button>
             </div >
         )
     }
